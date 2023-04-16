dc44cabc949c7435109f8f0f99f3ab1f555cd263
mucahit <mucahit@mucahit-Air.station>
2023-04-16 13:55:13 +0300

    varsayılan dilin direkt seçili gelme olayı düzenlendi.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import {TranslateService} from "@ngx-translate/core";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  selected: any;
 
 
   constructor(public translate: TranslateService) {
@@ -14,6 +15,7 @@ export class AppComponent implements OnInit {
     translate.setDefaultLang('en');
 
     const browserLang: any = translate.getBrowserLang();
+    this.selected = browserLang
     translate.use(browserLang.match(/en|tr/) ? browserLang : 'en');
   }
 
